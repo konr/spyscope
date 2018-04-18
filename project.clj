64e9cc24280ae030bc10b456c40fbe8e8ff0f6d4
Konrad Scorciapino <scorciapino@gmail.com>
2018-04-18 07:39:01 -0300

    use temporary version for testing

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject spyscope "0.1.7-SNAPSHOT"
+(defproject spyscope "0.1.8-KONR"
   :description "Trace-oriented debugging tools for Clojure"
   :url "http://github.com/dgrnbrg/spyscope"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories  [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.12.2"]
